69052e9b98d687539b41e049e713df7085b88cbd
kim youngchan <kimynam1@naver.com>
2020-03-24 17:38:39 +0900

    메인 첫 화면 작업

---
+++ js/common.js
@@ -0,0 +1,43 @@
+const boxMoving = () =>{
+    const boxWrap = document.querySelector('.fix-box'),
+        box = boxWrap.querySelectorAll('div');
+    let currentBox = 0;
+        function boxAnimation(){
+            box[currentBox].classList.add('on');
+            currentBox++;
+            if(currentBox == box.length){
+                clearInterval(boxLoop)
+            }
+        }
+        const boxLoop = setInterval(boxAnimation , 200);
+}
+const contentScroll = () =>{
+    const contentWrap = document.querySelector('.wrap'),
+        width = contentWrap.offsetWidth,
+        scroll = document.querySelector('.scroll-bar'),
+        scrollBg = scroll.querySelector('span');
+    return {
+        init : function(){
+            console.log(width);
+            this.scrollBar()
+        },
+        scrollBar : function(){
+
+        }
+    }
+}
+const scroll = contentScroll();
+const init = ()=>{
+    boxMoving();
+    scroll.init();
+}
+setTimeout(init , 3000);
+
+// const text = document.querySelectorAll('.line p');
+// const wrap = document.querySelector('.wrap');
+// const scroll = document.addEventListener('scroll', ()=>{
+//     let top = window.scrollY /3;
+//     for(let i = 0; i < text.length; i++){
+//         text[i].style.transform  = "translateY("+ -top+ "px)";
+//     }
+// })
