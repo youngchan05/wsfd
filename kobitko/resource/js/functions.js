e75e16c32941d21a91075e3cd4cb3f183629cfc2
younghcan <kimynam1@naver.com>
2020-05-18 13:30:34 +0900

    테스트 업로드

--- kobitko/resource/js/functions.js
+++ kobitko/resource/js/functions.js
@@ -8,4 +8,9 @@ $(function () {
 		$(this).addClass('active').siblings().removeClass('active');
 		$tabCont.eq($idx).addClass('active').siblings().removeClass('active');
 	})
+	$("[data-selector]").on("click", function () {
+		$targetName = $(this).data("selector");
+		$(this).addClass('active').siblings().removeClass('active');
+		$("[data-target=" + $targetName + "]").addClass("active").siblings().removeClass('active');
+	});
 })
